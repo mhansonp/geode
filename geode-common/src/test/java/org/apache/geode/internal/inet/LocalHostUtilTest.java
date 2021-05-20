e36d9da6723ab9ddd0b8f23547202b11ea467de2
Mark Hanson <hansonm@vmware.com>
2021-05-19 23:13:30 -0700

    logging and a potential fix..

--- geode-common/src/test/java/org/apache/geode/internal/inet/LocalHostUtilTest.java
+++ geode-common/src/test/java/org/apache/geode/internal/inet/LocalHostUtilTest.java
@@ -1,12 +1,15 @@
 package org.apache.geode.internal.inet;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.in;
 
 import java.net.Inet6Address;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.Set;
+
+import javax.naming.spi.Resolver;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -21,17 +24,40 @@ public class LocalHostUtilTest {
 
   @Test
   public void testGetMyAddresses() throws SocketException {
-    Set<InetAddress> myAddresses =  LocalHostUtil.getMyAddresses();
+    Set<InetAddress> myAddresses = LocalHostUtil.getMyAddresses();
     for (InetAddress address : myAddresses) {
       assertThat(address.isAnyLocalAddress()).isFalse();
       assertThat(address.isLinkLocalAddress()).isFalse();
       assertThat(address.isLoopbackAddress()).isFalse();
-      if(address instanceof Inet6Address) {
+      if (address instanceof Inet6Address) {
         Inet6Address inet6Address = (Inet6Address) address;
         assertThat(inet6Address.getScopedInterface().isLoopback()).isFalse();
         assertThat(inet6Address.getHostAddress().contains("%lo")).isFalse();
       }
     }
   }
+  @Test
+  public void testIsLocalHost() throws SocketException, UnknownHostException {
 
+    InetAddress inet6Address = InetAddress.getByAddress("", new byte[]{0,0,0,0,0,0,0,0,0,0,
+        (byte) 0xff,
+        (byte) 0xff,0x7f,00,0,1});
+    InetSocketAddress inet6Address1 = new InetSocketAddress("0:0:0:0:0:ffff:7f00:1%lo", 29298);
+    assertThat(LocalHostUtil.isLocalHost(inet6Address)).isTrue();
+    assertThat(LocalHostUtil.isLocalHost(inet6Address1)).isTrue();
+    assertThat(inet6Address1.getAddress().isLoopbackAddress()).isTrue();
+      assertThat(inet6Address.isLoopbackAddress()).isTrue();
+////        InetSocketAddress inetSocketAddress =  new InetSocketAddress(, 29298));
+//    Set<InetAddress> myAddresses = LocalHostUtil.getMyAddresses();
+//    for (InetAddress address : myAddresses) {
+//      assertThat(address.isAnyLocalAddress()).isFalse();
+//      assertThat(address.isLinkLocalAddress()).isFalse();
+//      assertThat(address.isLoopbackAddress()).isFalse();
+//      if (address instanceof Inet6Address) {
+//        Inet6Address inet6Address = (Inet6Address) address;
+//        assertThat(inet6Address.getScopedInterface().isLoopback()).isFalse();
+//        assertThat(inet6Address.getHostAddress().contains("%lo")).isFalse();
+//      }
+//    }
+  }
 }
