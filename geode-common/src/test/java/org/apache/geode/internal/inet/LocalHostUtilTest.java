8154cdbd217303bcfe27f88201a4d61d94769860
Mark Hanson <hansonm@vmware.com>
2021-05-19 23:30:52 -0700

    logging and a potential fix..

--- geode-common/src/test/java/org/apache/geode/internal/inet/LocalHostUtilTest.java
+++ geode-common/src/test/java/org/apache/geode/internal/inet/LocalHostUtilTest.java
@@ -37,27 +37,18 @@ public class LocalHostUtilTest {
     }
   }
   @Test
-  public void testIsLocalHost() throws SocketException, UnknownHostException {
+  public void testIsLocalHost() {
 
-    InetAddress inet6Address = InetAddress.getByAddress("", new byte[]{0,0,0,0,0,0,0,0,0,0,
-        (byte) 0xff,
-        (byte) 0xff,0x7f,00,0,1});
-    InetSocketAddress inet6Address1 = new InetSocketAddress("0:0:0:0:0:ffff:7f00:1%lo", 29298);
-    assertThat(LocalHostUtil.isLocalHost(inet6Address)).isTrue();
-    assertThat(LocalHostUtil.isLocalHost(inet6Address1)).isTrue();
-    assertThat(inet6Address1.getAddress().isLoopbackAddress()).isTrue();
-      assertThat(inet6Address.isLoopbackAddress()).isTrue();
-////        InetSocketAddress inetSocketAddress =  new InetSocketAddress(, 29298));
-//    Set<InetAddress> myAddresses = LocalHostUtil.getMyAddresses();
-//    for (InetAddress address : myAddresses) {
-//      assertThat(address.isAnyLocalAddress()).isFalse();
-//      assertThat(address.isLinkLocalAddress()).isFalse();
-//      assertThat(address.isLoopbackAddress()).isFalse();
-//      if (address instanceof Inet6Address) {
-//        Inet6Address inet6Address = (Inet6Address) address;
-//        assertThat(inet6Address.getScopedInterface().isLoopback()).isFalse();
-//        assertThat(inet6Address.getHostAddress().contains("%lo")).isFalse();
-//      }
-//    }
+    Set<InetAddress> myAddresses = LocalHostUtil.getMyAddresses();
+    for (InetAddress address : myAddresses) {
+      if (address instanceof Inet6Address) {
+        Inet6Address inet6Address = (Inet6Address) address;
+        if(inet6Address.getHostAddress().contains("%lo")) {
+          assertThat(LocalHostUtil.isLocalHost(inet6Address)).isFalse();
+        } else {
+          assertThat(inet6Address.getHostAddress().contains("%lo")).isFalse();
+        }
+      }
+    }
   }
 }
